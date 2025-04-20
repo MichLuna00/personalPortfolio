6730a15182e7bbbf43dfaea3b2224c27337b74fb
MichLuna00 <164284158+MichLuna00@users.noreply.github.com>
2025-04-19 20:02:31 -0500

    Delete portafolio directory

--- portafolio/assets/js/helpers/date_updater.js
+++
@@ -1,9 +0,0 @@
-function updateCopyrightYear() {
-	const currentYear = new Date().getFullYear();
-
-	const copyRightElementDOM = document.querySelector('.footer__copy span');
-
-	copyRightElementDOM.textContent = `@${currentYear}`;
-}
-
-export default updateCopyrightYear;
